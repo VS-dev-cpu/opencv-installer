ed4b7e0806a207067f79fe3c9b5ae8c97b26e87e
VS-dev-cpu <77497287+VS-dev-cpu@users.noreply.github.com>
2021-02-15 15:18:06 +0100

    Update opencv-installer.py

--- opencv-installer.py
+++ opencv-installer.py
@@ -7,30 +7,46 @@ commands = ["sudo rpi-eeprom-update", "sudo rpi-eeprom-update -a", "sudo reboot"
             "unzip opencv.zip", "unzip opencv_contrib.zip", "mv opencv-4.5.0 opencv", "mv opencv_contrib-4.5.0 opencv_contrib", "echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.7" >> ~/.bashrc",
             "source ~/.bashrc", "sudo pip3 install virtualenv", "sudo pip3 install virtualenvwrapper", "echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc",
             "echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc", "source ~/.bashrc", "mkvirtualenv cv450", "pip3 install numpy", "cd ~/opencv/", "mkdir build", "cd build",
-            "cmake -D CMAKE_BUILD_TYPE=RELEASE \
-        -D CMAKE_INSTALL_PREFIX=/usr/local \
-        -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-        -D ENABLE_NEON=ON \
-        -D ENABLE_VFPV3=ON \
-        -D WITH_OPENMP=ON \
-        -D BUILD_TIFF=ON \
-        -D WITH_FFMPEG=ON \
-        -D WITH_TBB=ON \
-        -D BUILD_TBB=ON \
-        -D BUILD_TESTS=OFF \
-        -D WITH_EIGEN=OFF \
-        -D WITH_V4L=ON \
-        -D WITH_LIBV4L=ON \
-        -D WITH_VTK=OFF \
-        -D WITH_QT=OFF \
-        -D OPENCV_ENABLE_NONFREE=ON \
-        -D INSTALL_C_EXAMPLES=OFF \
-        -D INSTALL_PYTHON_EXAMPLES=OFF \
-        -D BUILD_NEW_PYTHON_SUPPORT=ON \
-        -D BUILD_opencv_python3=TRUE \
-        -D OPENCV_GENERATE_PKGCONFIG=ON \
+            "cmake -D CMAKE_BUILD_TYPE=RELEASE \\
+        -D CMAKE_INSTALL_PREFIX=/usr/local \\
+        -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \\
+        -D ENABLE_NEON=ON \\
+        -D ENABLE_VFPV3=ON \\
+        -D WITH_OPENMP=ON \\
+        -D BUILD_TIFF=ON \\
+        -D WITH_FFMPEG=ON \\
+        -D WITH_TBB=ON \\
+        -D BUILD_TBB=ON \\
+        -D BUILD_TESTS=OFF \\
+        -D WITH_EIGEN=OFF \\
+        -D WITH_V4L=ON \\
+        -D WITH_LIBV4L=ON \\
+        -D WITH_VTK=OFF \\
+        -D WITH_QT=OFF \\
+        -D OPENCV_ENABLE_NONFREE=ON \\
+        -D INSTALL_C_EXAMPLES=OFF \\
+        -D INSTALL_PYTHON_EXAMPLES=OFF \\
+        -D BUILD_NEW_PYTHON_SUPPORT=ON \\
+        -D BUILD_opencv_python3=TRUE \\
+        -D OPENCV_GENERATE_PKGCONFIG=ON \\
         -D BUILD_EXAMPLES=OFF ..", "make -j4", "sudo make install", "sudo ldconfig", "make clean", "sudo apt-get update", "cd ~", "rm opencv.zip", "rm opencv_contrib.zip", "sudo reboot",
             "cd ~/.virtualenvs/cv450/lib/python3.7/site-packages", "ln -s /usr/local/lib/python3.7/site-packages/cv2/python-3.7/cv2.cpython-37m-arm-linux-gnueabihf.so",
             "cd ~", "cd ~/opencv/build/lib/", "sudo cp cv2.so /usr/local/lib/python2.7/dist-packages/cv2/python-2.7", 
             "cd ~/opencv/build/lib/python3", "sudo cp cv2.cpython-37m-arm-linux-gnueabihf.so \ /usr/local/lib/python3.7/dist-packages/cv2/python-3.7", "cd ~/opencv",
             "sudo rm -r build", "sudo rm -rf ~/opencv", "sudo rm -rf ~/opencv_contrib"]
+
+f = open("install.dat", "r")
+
+num = f.read()
+
+f.close()
+
+while (True):
+            if(num == 3):
+                        f = open("install.dat", "w")
+                        f.write("4")
+                        f.close()
+                        
+            else:
+                        while (len(commands) - num != 0):
+                                    
